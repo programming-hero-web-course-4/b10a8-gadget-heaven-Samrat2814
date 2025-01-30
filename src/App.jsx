d2cf16124c71ff33666232b7fe2a7c3783302232
Samrat2814 <aslamhossainsamrat@gmail.com>
2025-01-30 18:30:59 +0600

    fourth commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@
 const App = () => {
   return (
     <div>
-      <h1 className='underline'>hello</h1>
+      
     </div>
   );
 };
--- src/Pages/Statistics.jsx
+++ src/Pages/Statistics.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+
 import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from "recharts";
 import Banner2 from "../Components/Banner2";
 
